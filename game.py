b02890076923576c468c2c06b3bb5616751d32c2
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-02-23 10:07:51 -0500

    Turns the game code into an object/class.
    Allows the class/game to be called.

--- game.py
+++ game.py
@@ -2,18 +2,29 @@ import sys
 
 import pygame
 
-pygame.init()
 
-pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
-screen = pygame.display.set_mode((1250, 675))  # Creates game window.
+class Game:  # Turns the game code into an object.
+    def __init__(self):
 
-clock = pygame.time.Clock()  # Restricts framerate to a fixed amount.
+        pygame.init()
 
-while True:
-    for event in pygame.event.get():  # Gets user input.
-        if event.type == pygame.QUIT:  # Allows user to exit out of the game.
-            pygame.quit()
-            sys.exit()
+        pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
+        self.screen = pygame.display.set_mode((1250, 675))  # Creates game
+        # window.
 
-    pygame.display.update()  # Updates the screen to allow things to display.
-    clock.tick(60)  # sets framerate to 60 FPS.
+        self.clock = pygame.time.Clock()  # Restricts framerate to a fixed
+        # amount.
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():  # Gets user input.
+                if event.type == pygame.QUIT:  # Allows user to exit out of
+                    # the game.
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()  # Updates the screen to allow things to
+            # display.
+            self.clock.tick(60)  # sets framerate to 60 FPS.
+
+Game().run()
