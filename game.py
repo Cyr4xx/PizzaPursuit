9206145892de8b924be7f484c10d6f347010dcc2
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-04-03 09:53:50 -0400

    Added collisions and Transparency in images

--- game.py
+++ game.py
@@ -11,9 +11,12 @@ class Game:  # Turns the game code into an object.
         self.timer = pygame.time.Clock()  # Restricts framerate to a fixed
         # amount. clock = timer
         self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0)) # Creates transparency in the image
+
         self.img_pos = [160, 260]  # Cloud position.
         self.movement = [False, False]  # Updates cloud movement depending on
         # character movement.
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
@@ -21,6 +24,13 @@ class Game:  # Turns the game code into an object.
 
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.window.blit(self.img, self.img_pos)  # Creates a cloud collage
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())# Makes a rectangle which matches the cloud to create collision.
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.window, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.window, (0, 50, 155),
+                                 self.collision_area) # draws the collision rectangle
 
             for event in pygame.event.get():  # Gets user input.
                 if event.type == pygame.QUIT:  # Allows user to exit out of
@@ -38,6 +48,7 @@ class Game:  # Turns the game code into an object.
                         self.movement[0] = False
                     if event.key == pygame.K_DOWN:
                         self.movement[1] = False
+
             pygame.display.update()  # Updates the screen to allow things to
             # display.
             self.timer.tick(60)  # sets framerate to 60 FPS.
