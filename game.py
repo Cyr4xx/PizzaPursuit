b4776aeeb8bf5d04a42fc244398ac56e19738f7e
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-04-09 10:07:13 -0400

    Added menu screen and button file which allows me to create buttons.

--- game.py
+++ game.py
@@ -1,31 +1,94 @@
 import pygame
 import sys
-
+from button import Button
 from Scripts.Entities import PhysicsEntity
+
+window = pygame.display.set_mode((1250, 675))  # Creates game
+        # window. screen = window
+pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
+
+bg = pygame.image.load("data/images/bg.png") #Menu background.
+
+def get_font(size):
+    return pygame.font.Font("assets/font.ttf", size)
+
+def play():
+    while True:
+        play_mouse_pos = pygame.mouse.get_pos()
+
+        window.fill('black')
+
+        play_text = get_font(45).render("Play ", True, "White")
+        play_rect = play_text.get_rect(center=(640, 260))
+        window.blit(play_text, play_rect)
+
+        play_back = Button(image=None, pos=(640, 460),
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        play_back.changeColor(play_mouse_pos)
+        play_back.update(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back.checkForInput(play_mouse_pos):
+                    main_menu()
+
+        pygame.display.update()
+
+def options():
+    while True:
+        options_mouse_pos = pygame.mouse.get_pos()
+
+        window.fill('white')
+
+def main_menu():
+    while True:
+        window.blit(bg, (0,0))
+
+        menu_mouse_pos = pygame.mouse.get_pos() # Tracks mouse position.
+
+        menu_text = get_font(100).render.("Pizza Pursuit", True, "#b68f40")
+        menu_rect = menu_text.get_rect(center=(640, 100)) # SHows the Title of the game.
+
+        play_button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+            # Creates a play button
+        options_button = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        quit_button = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        window.blit(menu_text, menu_rect)
+
+        for button in [play_button, options_button, quit_button]:
+            button.changeColor(menu_mouse_pos)
+            button.update(window)
+
+        for event in pygame.event.get()
+            if event.type == pygame.QUIT
+             pygame.quit()
+              sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.checkForInput(menu_mouse_pos):
+                  play()
+             if options_button.checkForInput(menu_mouse_pos):
+                 options()
+             if quit_button.checkForInput(menu_mouse_pos):
+                  pygame.quit()
+                  sys.exit()
+
+     pygame.display.update()
+
+main_menu()
 
 class Game:  # Turns the game code into an object.
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
-        self.window = pygame.display.set_mode((1250, 675))  # Creates game
-        # window. screen = window
         self.timer = pygame.time.Clock()  # Restricts framerate to a fixed
         # amount. clock = timer
-        self.bg = pygame.image.load("data/images/bg.png")
-     def main_menu(self):
-         pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
-
-         while True:
-             self.window.blit(self.bg, (0,0))
-
-             menu_mouse_pos = pygame.mouse.get_pos()
-
-             menu_text = get_font(100).render.("Pizza Pursuit", True, "#b68f40")
-             menu_rect = menu_text.get_rect(center=(640, 100))
-
-             play_button = Button()
-            
-
 
         self.img = pygame.image.load("data/images/clouds/cloud_1.png")
         self.img.set_colorkey((0, 0, 0))  # Creates transparency in the image
