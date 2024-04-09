a689a8a882115ad0f2f4e6568881192f7408fba4
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-04-09 12:04:10 -0400

    Seperated Main menu and game code into seperate files

---
+++ Main Menu.py
@@ -0,0 +1,124 @@
+import pygame
+import sys
+from button import Button
+
+
+pygame.init()
+window = pygame.display.set_mode((1250, 675))  # Creates game
+# window. screen = window
+pygame.display.set_caption("Pizza Pursuit: Chef's Revenge")
+
+bg = pygame.image.load("data/images/bg.png")  # Menu background.
+
+
+def get_font(size):
+    return pygame.font.Font("data/font.ttf", size)
+# Takes the font file I provided, and allows utilization of the fonts for
+# the menu screen.
+
+def play():
+    while True:
+        play_mouse_pos = pygame.mouse.get_pos() # Takes mouse position
+
+        window.fill('black')
+
+        play_text = get_font(45).render("Play ", True, "White")
+        play_rect = play_text.get_rect(center=(640, 260))
+        window.blit(play_text, play_rect)
+
+        play_back = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75),
+                           base_color="White", hovering_color="Green")
+
+        play_back.changeColor(play_mouse_pos)
+        play_back.update(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back.checkForInput(play_mouse_pos):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def options():
+    while True:
+        options_mouse_pos = pygame.mouse.get_pos()
+
+        window.fill('white')
+
+        options_text = get_font(45).render("Options.", True, "Black")
+        options_rect = options_text.get_rect(center=(640, 260))
+        window.blit(options_text, options_rect)
+
+        options_back = Button(image=None, pos=(640, 460), text_input="Back",
+                              font=get_font(75), base_color="Black",
+                              hovering_color="Green")
+
+        options_back.changeColor(options_mouse_pos)
+        options_back.update(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if options_back.checkForInput(options_mouse_pos):
+                    main_menu()
+
+            pygame.display.update()
+
+
+def main_menu():
+    while True:
+        window.blit(bg, (0, 0))
+
+        menu_mouse_pos = pygame.mouse.get_pos()  # Tracks mouse position.
+
+        menu_text = get_font(100).render('Pizza Pursuit', True, "#b68f40")
+        menu_rect = menu_text.get_rect(
+            center=(640, 100))  # SHows the Title of the game.
+
+        play_button = Button(image=pygame.image.load("data/Play Rect.png"),
+                             pos=(640, 250),
+                             text_input="PLAY", font=get_font(75),
+                             base_color="#d7fcd4", hovering_color="White")
+        # Creates a play button
+        options_button = Button(
+            image=pygame.image.load("data/Options Rect.png"), pos=(640, 400),
+            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4",
+            hovering_color="White")
+
+        quit_button = Button(image=pygame.image.load("data/Quit Rect.png"),
+                             pos=(640, 550),
+                             text_input="QUIT", font=get_font(75),
+                             base_color="#d7fcd4", hovering_color="White")
+        window.blit(menu_text, menu_rect)
+
+        for button in [play_button, options_button, quit_button]:
+            button.changeColor(menu_mouse_pos)
+            button.update(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.checkForInput(menu_mouse_pos):
+                    play()
+            if options_button.checkForInput(menu_mouse_pos):
+                options()
+            if quit_button.checkForInput(menu_mouse_pos):
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
+main_menu()
+
+
+
