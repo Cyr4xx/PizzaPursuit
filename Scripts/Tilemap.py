01bc103ab62d32269aae21ff18d8ac94282e7758
Noh Daniel <nohdaniel246@gmail.com>
2024-04-11 09:25:45 -0400

    gt
    erkgjnf

--- Scripts/Tilemap.py
+++ Scripts/Tilemap.py
@@ -1,5 +1,10 @@
 class TileMap:
     def __init__(self, tile_size=16):
-        self.tileSize = tile_size
-        self.tileMap = {}
+        self.tile_size = tile_size
+        self.tile_map = {}
         self.offGrid_tiles = []
+
+        for i in range(10):
+            self.tile_map[str(3+i) + ';10'] = {'type': 'grass', 'variant': 1,'pos': (3+i, 10)}
+            self.tile_map[';10' + str(i+5)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
