65bc5c7fbfa4f28c0d9ff9d9225beef089d5ab4d
Peter Alexander Migliore <47260616+GamerNetwork4@users.noreply.github.com>
2024-04-11 09:42:16 -0400

    instructions

--- Scripts/tilemap.py
+++ Scripts/tilemap.py
@@ -8,3 +8,6 @@ class TileMap:
             self.tile_map[str(3+i) + ';10'] = {'type': 'grass', 'variant': 1,'pos': (3+i, 10)}
             self.tile_map[';10' + str(i+5)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
+
+
+# When def render is written add offset=(0,0) into the brackets
